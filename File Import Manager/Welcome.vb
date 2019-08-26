80d86b62e93dd7403b96dc772e127e9b0fece629
Giles Brule <gilesbrule@outlook.com>
2019-08-26 09:43:23 -0400

    2ed test

--- File Import Manager/Welcome.vb
+++ File Import Manager/Welcome.vb
@@ -22,4 +22,5 @@
     Private Sub Welcome_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
         My.Settings.Debug = "c"
     End Sub 'Form1_Closing
+    'test 2
 End Class
